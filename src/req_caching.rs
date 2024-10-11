617861e4bb75daa502c75465d23671e404011c63
HANDZCZ <handz@email.cz>
2024-10-11 22:24:25 +0200

    Make cached get_json return reference

--- src/req_caching.rs
+++ src/req_caching.rs
@@ -2,6 +2,7 @@ use std::{
     any::{Any, TypeId},
     collections::HashMap,
     marker::PhantomData,
+    ops::Deref,
     sync::{Arc, LazyLock},
 };
 
@@ -47,20 +48,21 @@ impl<T: Send + 'static> CacheEntry<T> {
     pub fn set(&mut self, val: T) {
         *self.inner = Some(Box::new(val));
     }
+
+    pub fn exists(&self) -> bool {
+        (&*self.inner).is_some()
+    }
 }
 
 impl Cache {
-    fn get_value_guard<T: Send + 'static + Clone>(
-        &self,
-        key: String,
-    ) -> Arc<Mutex<Option<Box<dyn Any + Send>>>> {
+    fn get_value_guard<T: 'static>(&self, key: String) -> Arc<Mutex<Option<Box<dyn Any + Send>>>> {
         let mut guard = self.inner.lock().unwrap();
         let hash_map = &mut *guard;
         let key = (key, TypeId::of::<T>());
         hash_map.entry(key).or_default().clone()
     }
 
-    pub async fn entry<T: Send + 'static + Clone>(&self, key: String) -> CacheEntry<T> {
+    pub async fn entry<T: Send + 'static>(&self, key: String) -> CacheEntry<T> {
         let data_guard = self.get_value_guard::<T>(key);
         let data_guard = data_guard.lock_owned().await;
         CacheEntry {
@@ -70,26 +72,36 @@ impl Cache {
     }
 }
 
-pub async fn get_json<T: DeserializeOwned + 'static + Clone + Send>(
+pub async fn get_json<T: DeserializeOwned + 'static + Send>(
     req_client: &reqwest::Client,
     url: &str,
     on_request_error: ErrorAction,
     on_decode_error: ErrorAction,
-) -> Result<T, HttpResponse> {
+) -> Result<RefVal<T>, HttpResponse> {
     let mut entry = CACHE.entry::<T>(url.to_string()).await;
-    if let Some(data) = entry.get() {
-        return Ok(data.clone());
+    if entry.exists() {
+        return Ok(RefVal(entry));
     }
 
     let response = req_client.get(url).send().await;
     match response {
         Ok(res) => match res.json::<T>().await {
             Ok(data) => {
-                entry.set(data.clone());
-                Ok(data)
+                entry.set(data);
+                Ok(RefVal(entry))
             }
             Err(_) => Err(on_decode_error.into_response()),
         },
         Err(_) => Err(on_request_error.into_response()),
+    }
+}
+
+pub struct RefVal<T>(CacheEntry<T>);
+
+impl<T: Send + 'static> Deref for RefVal<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0.get().unwrap()
     }
 }
