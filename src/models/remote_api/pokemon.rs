becc3609ac2f1e4518d01a070f64ab881081b96f
HANDZCZ <handz@email.cz>
2024-10-11 21:36:27 +0200

    Make remote api Pokemon model clonable

--- src/models/remote_api/pokemon.rs
+++ src/models/remote_api/pokemon.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 use super::ApiPokemonSprites;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct ApiPokemon {
     pub name: String,
     pub sprites: ApiPokemonSprites,
--- src/models/remote_api/pokemon_sprites.rs
+++ src/models/remote_api/pokemon_sprites.rs
@@ -1,17 +1,17 @@
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct ApiPokemonSprites {
     pub other: ApiPokemonSpritesOther,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct ApiPokemonSpritesOther {
     #[serde(rename = "official-artwork")]
     pub official_artwork: ApiPokemonSpritesOtherOfficialArtwork,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct ApiPokemonSpritesOtherOfficialArtwork {
     pub front_default: String,
     pub front_shiny: String,
