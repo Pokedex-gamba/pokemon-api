d32d56623514f18dbc7dc010d90324f2f2eedf81
HANDZCZ <handz@email.cz>
2024-10-16 00:53:32 +0200

    Rewrite request caching

--- src/req_caching.rs
+++ src/req_caching.rs
@@ -121,29 +121,80 @@ impl Cache {
     }
 }
 
-pub async fn get_json<T: DeserializeOwned + Send + Sync + 'static, E>(
+pub async fn handle_request<D: Serialize, T: DeserializeOwned + Send + Sync + 'static, E>(
     req_client: &reqwest::Client,
+    method: reqwest::Method,
     url: &str,
+    data: Option<&D>,
+    on_error: impl Fn(reqwest::Error) -> E,
+) -> Result<T, E> {
+    let mut request = req_client.request(method, url);
+    if let Some(data) = data {
+        request = request.json(data);
+    }
+    let response = request.send().await;
+    match response.and_then(reqwest::Response::error_for_status) {
+        Ok(res) => match res.json::<T>().await {
+            Ok(data) => Ok(data),
+            Err(e) => Err((on_error)(e)),
+        },
+        Err(e) => Err((on_error)(e)),
+    }
+}
+
+pub async fn handle_cache_request<D: Serialize, T: DeserializeOwned + Send + Sync + 'static, E>(
+    req_client: &reqwest::Client,
+    method: reqwest::Method,
+    cache_key: String,
+    url: &str,
+    data: Option<&D>,
     on_error: impl Fn(reqwest::Error) -> E,
 ) -> Result<RefVal<T>, E> {
-    let entry = CACHE.entry::<T>(url.to_string()).await;
+    let entry = CACHE.entry::<T>(cache_key).await;
     let mut data_lock = match entry.get_or_write_lock().await {
         Either::Left(data) => return Ok(data),
         Either::Right(write_lock) => write_lock,
     };
 
-    let response = req_client.get(url).send().await;
-    match response.and_then(reqwest::Response::error_for_status) {
-        Ok(res) => match res.json::<T>().await {
-            Ok(data) => {
-                data_lock.set(data);
-                drop(data_lock);
-                Ok(RefVal(entry.read().await))
-            }
-            Err(e) => Err((on_error)(e)),
-        },
-        Err(e) => Err((on_error)(e)),
-    }
+    let data = handle_request(req_client, method, url, data, on_error).await?;
+    data_lock.set(data);
+    drop(data_lock);
+    Ok(RefVal(entry.read().await))
+}
+
+#[allow(dead_code)]
+pub async fn get_json<T: DeserializeOwned + Send + Sync + 'static, E>(
+    req_client: &reqwest::Client,
+    url: &str,
+    on_error: impl Fn(reqwest::Error) -> E,
+) -> Result<RefVal<T>, E> {
+    handle_cache_request(
+        req_client,
+        reqwest::Method::POST,
+        url.to_string(),
+        url,
+        Option::<&()>::None,
+        on_error,
+    )
+    .await
+}
+
+pub async fn post_json_cached<T: DeserializeOwned + Send + Sync + 'static, E>(
+    req_client: &reqwest::Client,
+    cache_key: impl Into<String>,
+    url: &str,
+    data: &impl Serialize,
+    on_error: impl Fn(reqwest::Error) -> E,
+) -> Result<RefVal<T>, E> {
+    handle_cache_request(
+        req_client,
+        reqwest::Method::POST,
+        cache_key.into(),
+        url,
+        Some(data),
+        on_error,
+    )
+    .await
 }
 
 pub struct RefVal<T>(ReadCacheEntryValue<T>);
